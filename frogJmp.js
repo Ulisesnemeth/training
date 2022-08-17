6d714a218605344707d3d757101e09b69550414f
Ulises Nemeth <ulisesnemeth@gmail.com>
2022-08-17 21:54:42 +0000

    11%

---
+++ frogJmp.js
@@ -0,0 +1,10 @@
+function solution(X, Y, D) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    var jumpcounter=0
+    while(X<=Y){
+        X=X+D
+        jumpcounter++
+    }
+    return jumpcounter
+}
+console.log(solution(10, 85, 30))
